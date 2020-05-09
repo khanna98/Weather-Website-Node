b7e8d330c34ef366b7e5da28a255739937db8e36
Mayank Khanna <khanna98@outlook.com>
2020-05-09 10:00:32 +0530

    Added author name in Footer

--- src/app.js
+++ src/app.js
@@ -34,7 +34,7 @@ app.get('', (req, res) => {
 app.get('/about', (req, res) => {
     res.render('about', {
         title: "About Me",
-        name: "Mayank"
+        name: "Mayank Khanna"
     })
 })
 
